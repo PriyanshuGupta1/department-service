9abd3be14dc002eae38a455bb2368b91d769296d
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2023-02-17 12:28:56 +0530

    DepartmentService

---
+++ src/main/java/com/springboot/departmentservice/service/DepartmentService.java
@@ -0,0 +1,13 @@
+package com.springboot.departmentservice.service;
+
+import com.springboot.departmentservice.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+
+}
